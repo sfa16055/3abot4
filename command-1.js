413c5d9fda221698b96732eecac31a0e3162b8b9
sfa16055 <48595250+sfa16055@users.noreply.github.com>
2021-08-03 14:45:09 +0800

    Create command-1.js

---
+++ command-1.js
@@ -0,0 +1,17 @@
+// authenticates you with the API standard library
+if ((created = true)) {
+    const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+  
+    await lib.discord.channels['@0.1.0'].messages.create({
+      channel_id: context.params.event.channel_id,
+      content: `Factory demolished and all your stats have been reset.`
+    });
+    created = false
+  } else if ((created = false)){
+    const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+    
+    await lib.discord.channels['@0.1.0'].messages.create({
+      channel_id: context.params.event.channel_id,
+      content: `You don't even have a factory!'`
+    });
+  }
