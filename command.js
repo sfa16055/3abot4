7cbf0f75691405256bfdac64697b4db48501a2e4
sfa16055 <48595250+sfa16055@users.noreply.github.com>
2021-08-03 14:44:47 +0800

    Create command.js

---
+++ command.js
@@ -0,0 +1,19 @@
+// authenticates you with the API standard library
+// type `await lib.` to display API autocomplete
+let created = false
+if ((created = false)) {
+  const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+  await lib.discord.channels['@0.1.0'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `**YOU SUCCESSFULLY BUILT YOUR FACTORY**\nAutomating commands is **NOT** allowed. Do the command "/factory" to check your factory.`
+  });
+} else if ((created = true)) {
+  const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+  
+  await lib.discord.channels['@0.1.0'].messages.create({
+    channel_id: context.params.event.channel_id,
+    content: `**INVALID**\nYour factory already exists.`
+  });
+  created = true
+}
